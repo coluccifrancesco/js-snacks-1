ca48bed1d2745ae50f31b53573345f3989448929
coluccifrancesco <coluccif2002@gmail.com>
2025-05-30 11:49:00 +0200

    Add initial HTML structure and JavaScript for Snack 3

---
+++ snack_3/js/main.js
@@ -0,0 +1,6 @@
+//  http://127.0.0.1:3000/live Coding/js-snacks-1/snack_3/index.html
+
+// software deve chiedere per 10 volte di inserire un numero
+
+
+// programma stampa la somma dei numeri inseriti
