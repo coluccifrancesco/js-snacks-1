378bdf5fccfe5aab524c8ff7c4b9192e1cb71a62
coluccifrancesco <coluccif2002@gmail.com>
2025-05-30 11:34:31 +0200

    Remove unnecessary comment from main.js

--- snack_2/js/main.js
+++ snack_2/js/main.js
@@ -1,5 +1,3 @@
-//  http://127.0.0.1:3000/live Coding/js-snacks-1/snack_2/index.html
-
 //  due prompt testuali
 const parolaUno = prompt("Inserisci una parola");
 const parolaDue = prompt("Inserisci un'altra parola");
