105cee670c351da08e13374bf98b5cc5fdd01b8e
coluccifrancesco <coluccif2002@gmail.com>
2025-05-30 12:58:32 +0200

    Fix formatting and add missing newlines in main.js then finishing snack 5.2

--- snack_5/js/main.js
+++ snack_5/js/main.js
@@ -5,7 +5,18 @@ let nChoice = Number(prompt('Inserisci un numero'))
 
 for (let i = 1; i < 6; i++){
     let numberTimes = ((nChoice * nChoice) * i);
-    console.log(numberTimes)
+    console.log(numberTimes);
 }
 
 // stampa il cubo dei primi N numeri 
+
+console.log(' ');
+
+// stampa potenze di 2 fino a 100
+let numberTwo = 2;
+squareResult = null; 
+
+for(let i = 1; i < 1001; i++){
+    let squareResult = ((numberTwo * numberTwo) * i);
+    console.log(squareResult);
+}
