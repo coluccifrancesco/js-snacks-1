d28f189d268a084167df8e37190e554ec0153b7e
coluccifrancesco <coluccif2002@gmail.com>
2025-05-30 11:33:21 +0200

    Add functionality to compare two input words by length

--- snack_2/js/main.js
+++ snack_2/js/main.js
@@ -1,2 +1,14 @@
 //  http://127.0.0.1:3000/live Coding/js-snacks-1/snack_2/index.html
 
+//  due prompt testuali
+const parolaUno = prompt("Inserisci una parola");
+const parolaDue = prompt("Inserisci un'altra parola");
+
+//  software stampa prima la parola più corta poi più lunga
+if (parolaUno.length > parolaDue.length){
+    console.log(parolaDue + ' è più corta di ' + parolaUno)
+} else if (parolaUno.length < parolaDue.length){
+    console.log(parolaUno + '  è più corta di ' + parolaDue)
+} else {
+    console.log(parolaUno + ' è lunga come ' + parolaDue)
+}
