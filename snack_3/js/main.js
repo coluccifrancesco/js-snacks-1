54e7df9bc66d86df2f5957f7715d3dddc2b47e38
coluccifrancesco <coluccif2002@gmail.com>
2025-05-30 12:11:10 +0200

    Fix number input handling and sum calculation in main.js  TO FIX

--- snack_3/js/main.js
+++ snack_3/js/main.js
@@ -1,6 +1,23 @@
 //  http://127.0.0.1:3000/live Coding/js-snacks-1/snack_3/index.html
 
-// software deve chiedere per 10 volte di inserire un numero
+const numeri = []; 
+const somma = [];
 
+// software deve chiedere per 10 volte di inserire un numero
+for (let i = 0; i < 10; i++){
+    numeri.push = Number(prompt('Scegli un numero')); 
+}
 
 // programma stampa la somma dei numeri inseriti
+for (let i = 0; i < numeri.length; i++){
+    const numeriCounter = [i];
+    if (i == 0){
+        somma.unshift(i);
+    } else if(i >= 1){
+        somma.push(+ i);
+        somma.push();
+    } else {
+        break
+    }
+    console.log(somma)
+}
