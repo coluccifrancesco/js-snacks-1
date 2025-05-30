e04486378af3258f0a53e3beae0ccc2edd751020
coluccifrancesco <coluccif2002@gmail.com>
2025-05-30 10:51:23 +0200

    Convert prompt inputs to numbers for accurate comparison

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 //  http://127.0.0.1:3000/index.html
 
 //  Utente inserisce due numeri in successione
-const valueOne = prompt("Inserisci un valore");
-const valueTwo = prompt("Inserisci un altro valore");
-
-console.log(valueOne);
-console.log(valueTwo);
+const valueOne = Number(prompt("Inserisci un valore"));
+const valueTwo = Number(prompt("Inserisci un altro valore"));
 
 //  software stampa il maggiore
+if (valueOne > valueTwo){
+    console.log(valueOne);
+} else {
+    console.log(valueTwo);
+}
