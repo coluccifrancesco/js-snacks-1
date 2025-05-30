06dbc6bdfdff023a32e3700a0e8112344973b178
coluccifrancesco <coluccif2002@gmail.com>
2025-05-30 10:47:50 +0200

    Add initial HTML and JavaScript files for Snack 1 project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//  http://127.0.0.1:3000/index.html
+
+//  Utente inserisce due numeri in successione
+const valueOne = prompt("Inserisci un valore");
+const valueTwo = prompt("Inserisci un altro valore");
+
+console.log(valueOne);
+console.log(valueTwo);
+
+//  software stampa il maggiore
